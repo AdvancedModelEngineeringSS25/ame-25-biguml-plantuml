149029e99b240ae022cda45114857e77de177c3f
Lucas Wolkersdorfer <e11922587@student.tuwien.ac.at>
2025-04-07 19:01:59 +0200

    run action client side, print model state to console

--- client/packages/big-hello-world/src/glsp-client/hello-world.module.ts
+++ client/packages/big-hello-world/src/glsp-client/hello-world.module.ts
@@ -6,16 +6,16 @@
  *
  * SPDX-License-Identifier: MIT
  **********************************************************************************/
-import { configureActionHandler, FeatureModule } from '@eclipse-glsp/client';
+import { FeatureModule } from '@eclipse-glsp/client';
 import { ExtensionActionKind } from '@eclipse-glsp/vscode-integration-webview/lib/features/default/extension-action-handler.js';
-import { HelloWorldActionResponse, RequestHelloWorldAction } from '../common/hello-world.action.js';
-import { HelloWorldHandler } from './hello-world.handler.js';
+import { HelloWorldActionResponse } from '../common/hello-world.action.js';
+// import { HelloWorldHandler } from './hello-world.handler.js';
 
-export const helloWorldModule = new FeatureModule((bind, unbind, isBound, rebind) => {
-    const context = { bind, unbind, isBound, rebind };
+export const helloWorldModule = new FeatureModule(bind => {
+    // const context = { bind, unbind, isBound, rebind };
     // Register the HelloWorldHandler to handle the RequestHelloWorldAction
-    bind(HelloWorldHandler).toSelf().inSingletonScope();
-    configureActionHandler(context, RequestHelloWorldAction.KIND, HelloWorldHandler);
+    //bind(HelloWorldHandler).toSelf().inSingletonScope();
+    //configureActionHandler(context, RequestHelloWorldAction.KIND, HelloWorldHandler);
 
     // Allow the HelloWorldActionResponse to propagate to the server
     bind(ExtensionActionKind).toConstantValue(HelloWorldActionResponse.KIND);
